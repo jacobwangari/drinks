ff1717f578a479c42174c95b54932bab652f11e3
jacobwangari <mwasjac@gmail.com>
2023-07-04 13:09:40 +0300

    ALL crud operations added to the API

--- base/views.py
+++ base/views.py
@@ -22,3 +22,25 @@ def drink_list(request):
 			serializer.save()
 			return Response({'drinks':serializer.data}, status = status.HTTP_201_CREATED)
 
+@api_view(['GET','PUT','DELETE'])
+def drink_details(request,id):
+
+	try:
+		drink = Drink.objects.get(pk=id)
+	except Drink.DoesNotExist:
+		return Response(status=status.HTTP_404_NOT_FOUND)
+	if request.method == 'GET':
+		serializer = DrinkSerializer(drink)
+		return Response(serializer.data)
+	elif request.method == 'PUT':
+		serializer = DrinkSerializer(drink, data=request.data)
+
+		if serializer.is_valid():
+			serializer.save()
+			return Response(serializer.data)
+		return Response(serializer.errors, status = status.HTTP_400_BAD_REQUEST)
+	elif request.method == 'DELETE':
+		drink.delete()
+		return Response(status = status.HTTP_204_NO_CONTENT)
+
+
--- drinks/urls.py
+++ drinks/urls.py
@@ -6,4 +6,5 @@ from base import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('drinks/', views.drink_list),
+    path('drinks/<int:id>', views.drink_details)
 ]
